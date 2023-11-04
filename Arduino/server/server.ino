0565d1bcd3ef31314b4df726f25be49a35fc7976
nnmadalin <neauna.madalin2006@gmail.com>
2023-11-04 17:15:33 +0200

    server auth DB

--- Arduino/server/server.ino
+++ Arduino/server/server.ino
@@ -1,7 +1,11 @@
 #include "DHT.h"
 #include "WiFi.h"
 #include "ArduinoJson.h"
-#include "ESPAsyncWebSrv.h"
+#include "HTTPClient.h"
+#include "Preferences.h"
+#include "Crypto.h"
+#include "AESLib.h"
+
 
 
 #define DHTPIN 19
@@ -9,9 +13,13 @@
 #define MQPIN 34
 #define  POLpin1 23
 #define  POLpin2 22
-#define  AUTH_POST_ADD_DATA MjDzfovbksyIcIKmc2wTxE-guLfsxYNeM1Izr?MDt!U/p3ISW2zmda7djNTEmv6ku5YBlo6/qObat2jLirC1dTPHTFigWAcysLNFD7KgHQokqLYHmAV2XrOq-KoTRMSo
+
+byte key[16] = {'h', 'q', 'z', '2', 'q', 'b', '3', '1', '8', '1', 'y', 'q', 'b', 'x', 'i', 'r'};
 
 DHT dht(DHTPIN, DHTTYPE);
+
+AESLib aes;
+
 
 unsigned long duration1;
 unsigned long duration2;
@@ -25,13 +33,15 @@ float concentration1 = 0;
 float concentration2 = 0;
 
 DynamicJsonDocument data(1024);
+DynamicJsonDocument tokenJson(1024);
 String output;
 
-AsyncWebServer server(80);
+Preferences preferences;
 
 
 const char* ssid = "iPhone - Madalin";
 const char* password = "Madalin123";
+String token_now = "";
 
 void setup() {
   dht.begin();
@@ -40,31 +50,15 @@ void setup() {
   pinMode(MQPIN, OUTPUT); 
   pinMode(18,INPUT);
   pinMode(5,INPUT);
-  starttime = millis(); 
 
-  delay(1000);
-
-  int n = WiFi.scanNetworks();
-  Serial.println("scan done");
-  if (n == 0) {
-      Serial.println("no networks found");
-  } else {
-    Serial.print(n);
-    Serial.println(" networks found");
-    for (int i = 0; i < n; ++i) {
-      // Print SSID and RSSI for each network found
-      Serial.print(i + 1);
-      Serial.print(": ");
-      Serial.print(WiFi.SSID(i));
-      Serial.print(" (");
-      Serial.print(WiFi.RSSI(i));
-      Serial.print(")");
-      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
-      delay(10);
-    }
-  }
+  preferences.begin("credentials", false);
+  preferences.putString("token", "aZaV6wLfhEN4AdZXAlZ8PZMhw1rbHRn2C2OdkuA785RfrM02bKkUt95Bu1oB0YZX2M4n4atBfofdVxpWOVuqwLJqmmsOpzAdWVdUD2PuYyL4EboOGNnyedLOeZjIrHzr7Bt6qkEKJ0WvXuUDoJNTU0W2IGaByLcVg5h8hM0b8T5kdI9VOCspsF5ApmIIg8x5Ray71HwTNXy4rEZJoNFuteX5iwkcnXZQUlrM1PkDjQQWMlGKVeTw6YCGAfygEgj3"); 
+  
+  token_now = preferences.getString("token", "");
+  token_now = "aZaV6wLfhEN4AdZXAlZ8PZMhw1rbHRn2C2OdkuA785RfrM02bKkUt95Bu1oB0YZX2M4n4atBfofdVxpWOVuqwLJqmmsOpzAdWVdUD2PuYyL4EboOGNnyedLOeZjIrHzr7Bt6qkEKJ0WvXuUDoJNTU0W2IGaByLcVg5h8hM0b8T5kdI9VOCspsF5ApmIIg8x5Ray71HwTNXy4rEZJoNFuteX5iwkcnXZQUlrM1PkDjQQWMlGKVeTw6YCGAfygEgj3";
+  
   starttime = millis();
-  Serial.println("");
+  Serial.println(token_now);
 
   WiFi.begin(ssid, password);
   Serial.println("Conectare");
@@ -79,34 +73,16 @@ void setup() {
 
   Serial.println("Conectat la internet!");
   Serial.print("IP ESP: ");
-  
-  IPAddress local_IP(172, 20, 10, 2);
-  IPAddress gateway(172, 20, 10, 1);
-  IPAddress subnet(255, 255, 255, 240);
-  WiFi.config(local_IP, gateway, subnet);
   Serial.println(WiFi.localIP());
 
 
 
-  Serial.println("");Serial.println("");
+  Serial.println("");Serial.println("");  
 
- 
-  serializeJson(data, output);
-
-  server.on("/", HTTP_GET, [output](AsyncWebServerRequest *request){
-    request->send_P(200, "text/json",output.c_str());
-  });
-  server.begin();
-
+  pol();
 }
-void loop() {
-  duration1 = pulseIn(POLpin1, LOW);
-  duration2 = pulseIn(POLpin2, LOW);
-  lowpulseoccupancy1 = lowpulseoccupancy1+duration1;
-  lowpulseoccupancy2 = lowpulseoccupancy2+duration2;
 
-  if ((millis()-starttime) > sampletime_ms)
-  {
+void get_hum_temp_MQ(){
     float humi = dht.readHumidity();
     float tempC = dht.readTemperature();
     int MQvalue = analogRead(MQPIN);
@@ -116,14 +92,23 @@ void loop() {
     data["MQvalue"] = MQvalue;
 
     Serial.println("Umiditate: " + String(humi) + "   -   Temperatura: "  + String(tempC) + "   -   MQ value: "  + String(MQvalue));
+}
+
+void pol(){
+  duration1 = pulseIn(POLpin1, LOW);
+  duration2 = pulseIn(POLpin2, LOW);
+  lowpulseoccupancy1 = lowpulseoccupancy1+duration1;
+  lowpulseoccupancy2 = lowpulseoccupancy2+duration2;
+
+  if ((millis()-starttime) > sampletime_ms)
+  {
+    get_hum_temp_MQ();
 
     ratio1 = lowpulseoccupancy1/(sampletime_ms*10.0);  
     concentration1 = 1.1*pow(ratio1,3)-3.8*pow(ratio1,2)+520*ratio1+0.62; 
     ratio2 = lowpulseoccupancy2/(sampletime_ms*10.0); 
     concentration2 = 1.1*pow(ratio2,3)-3.8*pow(ratio2,2)+520*ratio2+0.62; 
-
-
-    Serial.println("concentration1: " + String(concentration1) + "   -   concentration2: "  + String(concentration2));
+    Serial.println("PM2.5: " + String(concentration1) + "   -   PM1.0: "  + String(concentration2));
 
     lowpulseoccupancy1 = 0;
     lowpulseoccupancy2 = 0;
@@ -132,7 +117,46 @@ void loop() {
     output = "";
     data["PM2.5"] = concentration1;
     data["PM1.0"] = concentration2;
-    serializeJson(data, output);
+
+    if(WiFi.status()== WL_CONNECTED){
+      HTTPClient http;   
+
+      data["mac"] = WiFi.macAddress();
+      data["token"] = token_now;
+
+
+      serializeJson(data, output);
+
+      http.begin("http://172.20.10.4:3003/values");  
+      http.addHeader("Content-Type", "application/json; charset=utf-8");        
+      
+      //Serial.print(output);
+      int httpResponseCode = http.POST(output);  
+      
+      if(httpResponseCode>0){
+      
+        String response = http.getString();                     
+
+        Serial.println(httpResponseCode);
+        Serial.println(response);
+
+        token_now = response;
+        preferences.putString("token", token_now);
+      }else{
+      
+        Serial.print("Error on sending POST: ");
+        Serial.println(httpResponseCode);
+      
+      }
+      
+      http.end();
+    }
+
+    
   }
+}
+
+void loop() {
+  pol();  
 
 }
--- backend/routes/values.js
+++ backend/routes/values.js
@@ -27,29 +27,48 @@ router.post('/', async function(req, res, next) {
 
     const bodyData = req.body;
     
-    if(bodyData.token != undefined && bodyData.token == process.env.AUTH_POST_ADD_DATA){
-      if(bodyData.uuid != undefined && bodyData.humidity != undefined && bodyData.temperature != undefined && bodyData.mqvalue != undefined && bodyData.pm25 != undefined 
-        && bodyData.pm1 != undefined &&
-        bodyData.uuid.trim() != "" && bodyData.humidity.trim() != "" && bodyData.temperature.trim() != "" && bodyData.mqvalue.trim() != "" && bodyData.pm25.trim() != "" 
-        && bodyData.pm1.trim() != ""
-        )
+    if(bodyData.token != undefined && bodyData.mac != undefined){
+      [rows] = await dataBase.query('SELECT * FROM devices_tokens where mac = ? and token = ?', [bodyData.mac, bodyData.token]);
+
+
+      if(Object.keys(rows).length === 0)
       {
-        await dataBase.execute('INSERT INTO devices_values (uuid, humidity, temperature, mqvalue, pm25, pm1, added_on) VALUES (?, ?, ?, ?, ?, ?, NOW())', [
-          bodyData.uuid,
-          bodyData.humidity,
-          bodyData.temperature,
-          bodyData.mqvalue,
-          bodyData.pm25,
-          bodyData.pm1,
-        ])
-        res.status(201).send(JSON.stringify({}));
+        res.status(401).send(JSON.stringify({error:"auth invalid"}));
       }
       else{
-        res.status(400).send(JSON.stringify({error:"JSON invalid"}));
+        if(bodyData.uuid != undefined && bodyData.humidity != undefined && bodyData.temperature != undefined && bodyData.mqvalue != undefined && bodyData.pm25 != undefined 
+          && bodyData.pm1 != undefined &&
+          bodyData.uuid.trim() != "" && bodyData.humidity.trim() != "" && bodyData.temperature.trim() != "" && bodyData.mqvalue.trim() != "" && bodyData.pm25.trim() != "" 
+          && bodyData.pm1.trim() != ""
+          )
+        {
+          await dataBase.execute('INSERT INTO devices_values (uuid, humidity, temperature, mqvalue, pm25, pm1, added_on) VALUES (?, ?, ?, ?, ?, ?, NOW())', [
+            bodyData.uuid,
+            bodyData.humidity,
+            bodyData.temperature,
+            bodyData.mqvalue,
+            bodyData.pm25,
+            bodyData.pm1,
+          ])
+
+          const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+          let randomToken = '';
+          for (let i = 0; i < 256; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length);
+            randomToken += characters[randomIndex];
+          }
+
+          await dataBase.execute('UPDATE devices_tokens SET token = ? where mac = ? and token = ?', [randomToken, bodyData.mac, bodyData.token]);
+
+          res.status(201).send(randomToken);
+        }
+        else{
+          res.status(400).send(JSON.stringify({error:"JSON invalid"}));
+        }
       }
     }
     else{
-      res.status(401).send(JSON.stringify({error:"auth invalid"}));
+      res.status(400).send(JSON.stringify({error:"JSON invalid"}));
     }
     
   }
